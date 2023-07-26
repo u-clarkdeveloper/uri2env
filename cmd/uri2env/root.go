627a5b2172de415eb66b5be77907cafaee6cdeb2
Jesse Clark <jesse.clark@davenportiowa.com>
2023-07-26 14:45:38 -0500

    Add: Basic call for testing

---
+++ cmd/uri2env/root.go
@@ -0,0 +1,34 @@
+package uni2env
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "uri2env",
+	Short: "uri2env is a tool to convert uri to env and back",
+	Long: `uri2env is a tool to convert uri to env and back.
+	For example:
+	> uri2env -p "MY_PREFIX" "http://userA:passwordA@localhost:8080/path/stuff/here?query=yes&testing=1"
+	MY_PREFIX_USER=userA
+	MY_PREFIX_PASSWORD=passwordA
+	MY_PREFIX_HOST=localhost
+	MY_PREFIX_PORT=8080
+	MY_PREFIX_PATH=/path/stuff/here
+	MY_PREFIX_QUERY=yes
+	MY_PREFIX_TESTING=1
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("uri2env")
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Whoops. There was an error while executing your CLI '%s'", err)
+		os.Exit(1)
+	}
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/u-clarkdeveloper/uri2env/cmd/uri2env"
+
+func main() {
+	uri2env.Execute()
+}
